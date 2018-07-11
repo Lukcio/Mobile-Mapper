4301fe17db94e39a2183a85fda4424efa91d2e11
Lucas Leschynski <mr.lukcio@gmail.com>
2018-07-11 10:57:39 -0500

    Added geocoder

--- Mobile Mapper/ViewController.swift
+++ Mobile Mapper/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     let northwesternAnnotation = MKPointAnnotation()
     let locationManager = CLLocationManager()
+    let address = "Mount Rushmore"
+    let geoCoder = CLGeocoder()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +28,14 @@ class ViewController: UIViewController {
         mapView.addAnnotation(northwesternAnnotation)
         locationManager.requestWhenInUseAuthorization()
         mapView.showsUserLocation = true
+        geoCoder.geocodeAddressString(address) { (placemarks, error) in
+            for placemark in placemarks! {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = (placemark.location?.coordinate)!
+                annotation.title = placemark.name
+                self.mapView.addAnnotation(annotation)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
